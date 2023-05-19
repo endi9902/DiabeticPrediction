83a05e368fef9e3fa837e23e9c74bb2f979696f1
kapj22z-11 <224371@edu.p.lodz.pl>
2023-05-19 11:20:00 +0200

    Implementing randomForest algorithm

--- randomForest.py
+++ randomForest.py
@@ -3,29 +3,26 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# Wczytanie danych treningowych z pliku CSV
+
 train_data = pd.read_csv('train.csv')
 
-# Wczytanie danych testowych z pliku CSV
 test_data = pd.read_csv('test.csv')
 
-# Podział danych treningowych na cechy (X) i etykiety (y)
 X_train = train_data.drop('Diabetes_012', axis=1)
 y_train = train_data['Diabetes_012']
 
-# Podział danych testowych na cechy (X) i etykiety (y)
+
 X_test = test_data.drop('Diabetes_012', axis=1)
 y_test = test_data['Diabetes_012']
 
-# Inicjalizacja modelu lasu losowego
 model = RandomForestClassifier()
 
-# Trenowanie modelu na danych treningowych
+
 model.fit(X_train, y_train)
 
-# Przewidywanie na danych testowych
+
 y_pred = model.predict(X_test)
 
-# Obliczenie dokładności modelu
-accuracy = accuracy_score(y_test, y_pred)
-print("Dokładność modelu:", accuracy)
+new_data = pd.DataFrame([[0, 40, 1, 25, 1, 1, 3, 2, 3, 2, 0]], columns=X_test.columns)
+prediction = model.predict(new_data)
+print("Przewidywany status cukrzycowy:", prediction)
